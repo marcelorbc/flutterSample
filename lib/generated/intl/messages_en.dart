3bf1c8ca3af5558755a4ea6b5e0558f2bad22232
Marcelo Bueno <marcelocampos@natura.net>
2021-09-07 14:24:57 -0300

    exit message

--- lib/generated/intl/messages_en.dart
+++ lib/generated/intl/messages_en.dart
@@ -21,8 +21,6 @@ class MessageLookup extends MessageLookupByLibrary {
 
   final messages = _notInlinedMessages(_notInlinedMessages);
   static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
-        "exitSession": MessageLookupByLibrary.simpleMessage(
-            "Do you really want to end this session ?"),
         "helloWorld": MessageLookupByLibrary.simpleMessage("Hellow world en"),
         "loginConsultantCode":
             MessageLookupByLibrary.simpleMessage("Consultante ID"),
